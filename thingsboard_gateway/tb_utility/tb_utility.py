4704ba75b3926fe69a42c31ba3a2a8752f7fb45c
jllanosf <je.llanosf@gmail.com>
2020-09-02 01:23:30 -0400

    Set Default Windows path for Dockerized TB on Windows

--- thingsboard_gateway/tb_utility/tb_utility.py
+++ thingsboard_gateway/tb_utility/tb_utility.py
@@ -75,13 +75,15 @@ class TBUtility:
     @staticmethod
     def check_and_import(extension_type, module_name):
         if TBUtility.loaded_extensions.get(extension_type + module_name) is None:
-            if system() == "Windows":
-                extensions_paths = (path.abspath(path.dirname(path.dirname(__file__)) + '/connectors/'.replace('/', path.sep) + extension_type.lower()),
-                                    path.abspath(path.dirname(path.dirname(__file__)) + '/extensions/'.replace('/', path.sep) + extension_type.lower()))
-            else:
-                extensions_paths = (path.abspath(path.dirname(path.dirname(__file__)) + '/connectors/'.replace('/', path.sep) + extension_type.lower()),
-                                    '/var/lib/thingsboard_gateway/extensions/'.replace('/', path.sep) + extension_type.lower(),
-                                    path.abspath(path.dirname(path.dirname(__file__)) + '/extensions/'.replace('/', path.sep) + extension_type.lower()))
+            extensions_paths = (path.abspath(path.dirname(path.dirname(__file__)) + '/connectors/'.replace('/', path.sep) + extension_type.lower()),
+                                path.abspath(path.dirname(path.dirname(__file__)) + '/extensions/'.replace('/', path.sep) + extension_type.lower()))
+            # if system() == "Windows":
+            #     extensions_paths = (path.abspath(path.dirname(path.dirname(__file__)) + '/connectors/'.replace('/', path.sep) + extension_type.lower()),
+            #                         path.abspath(path.dirname(path.dirname(__file__)) + '/extensions/'.replace('/', path.sep) + extension_type.lower()))
+            # else:
+            #     extensions_paths = (path.abspath(path.dirname(path.dirname(__file__)) + '/connectors/'.replace('/', path.sep) + extension_type.lower()),
+            #                         '/var/lib/thingsboard_gateway/extensions/'.replace('/', path.sep) + extension_type.lower(),
+            #                         path.abspath(path.dirname(path.dirname(__file__)) + '/extensions/'.replace('/', path.sep) + extension_type.lower()))
             try:
                 for extension_path in extensions_paths:
                     if path.exists(extension_path):
