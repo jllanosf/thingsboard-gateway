076ef8688b67bfc2ffde6182f6007053e4c1cb44
jllanosf <je.llanosf@gmail.com>
2020-09-06 23:04:06 -0300

    Testing Custom mqtt uplink converter

--- thingsboard_gateway/extensions/mqtt/custom_mqtt_uplink_converter.py
+++ thingsboard_gateway/extensions/mqtt/custom_mqtt_uplink_converter.py
@@ -12,6 +12,8 @@
 #     See the License for the specific language governing permissions and
 #     limitations under the License.
 
+import json
+import struct
 from simplejson import dumps
 from thingsboard_gateway.connectors.mqtt.mqtt_uplink_converter import MqttUplinkConverter, log
 
@@ -28,13 +30,37 @@ class CustomMqttUplinkConverter(MqttUplinkConverter):
             self.dict_result["telemetry"] = []  # template for telemetry array
             bytes_to_read = body.replace("0x", "")  # Replacing the 0x (if '0x' in body), needs for converting to bytearray
             converted_bytes = bytearray.fromhex(bytes_to_read)  # Converting incoming data to bytearray
+
+            output_dict = {}
+            output_dict["ts"] = 0
+            output_dict["values"] = {}
+
+            data_order = ["ts", "x001", "x002"]
+
             if self.__config.get("extension-config") is not None:
+                remainder=""
+                if 'tsBytes' in config["extension-config"]:
+                    chunk = bytes_to_read[:config["extension-config"]["tsBytes"]*2]
+                    remainder = bytes_to_read[config["extension-config"]["tsBytes"]*2:]
+                    ts_value = str(struct.unpack("l", bytearray.fromhex(chunk))[0])
+                    ts_value = int(ts_value[:13])
+                    output_dict["ts"] = ts_value
+                else:
+                    pass    #TODO: Throw exception on missing tsBytes
+
+                array_format = "d"*(len(config["extension-config"])-1)
+                #array_format = ""
+                #for _ in range(len(config["extension-config"])-1):    #iterate over number of objects excluding timestamp
+                #    array_format += "d"
+                data_array = struct.unpack(array_format, bytearray.fromhex(remainder))
+
                 for telemetry_key in self.__config["extension-config"]:  # Processing every telemetry key in config for extension
-                    value = 0
-                    for _ in range(self.__config["extension-config"][telemetry_key]):  # reading every value with value length from config
-                        value = value*256 + converted_bytes.pop(0)  # process and remove byte from processing
-                    telemetry_to_send = {telemetry_key.replace("Bytes", ""): value}  # creating telemetry data for sending into Thingsboard
-                    self.dict_result["telemetry"].append(telemetry_to_send)  # adding data to telemetry array
+                    struct_key = telemetry_key.replace("Bytes", "")
+                    if struct_key == "ts":
+                        pass
+                    else:
+                        output_dict["values"][struct_key] = data_array[data_order.index(struct_key)-1]
+                self.dict_result["telemetry"].append(output_dict)     # adding data to telemetry array
             else:
                 self.dict_result["telemetry"] = {"data": int(body, 0)}  # if no specific configuration in config file - just send data which received
             return self.dict_result
